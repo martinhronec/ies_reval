00f1d3bb4430e9bb0a006b026bb0aae46d11c101
Martin Hronec <hroneq@gmail.com>
2021-04-25 17:00:09 +0200

    Add template for seminar 10 livecoding.

---
+++ appraise.py
@@ -0,0 +1,42 @@
+import argparse
+import typing
+
+
+def get_real_estate_json(url):
+    pass
+
+def extract_area():
+    pass
+
+def extract_area_land():
+    pass
+
+def extract_gps_coordinates():
+    pass
+
+def train_model():
+    pass
+
+
+class Appraisor:
+
+    def __init__(self,url,area,area_land,gps_lat,gps_lon,model_path):
+        pass
+
+    def load_model():
+        pass
+    
+    def get_data_from_ad(self):
+        pass
+
+    def prepare_data(self):
+        pass
+
+    def predict(self):
+        pass
+
+
+if __name__ == "__main__":
+    # argument parsing from CLI
+    app = Appraisor()
+    app.predict()
