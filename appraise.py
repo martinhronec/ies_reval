1169a8e72cde0aa0fc813c7072d90de3657f1d00
Martin Hronec <hroneq@gmail.com>
2021-04-27 20:10:27 +0200

    Add skeleton code for the application.

--- appraise.py
+++ appraise.py
@@ -3,6 +3,8 @@ import typing
 import re
 import requests
 import json
+import catboost
+import argparse
 
 
 def get_real_estate_json(url):
@@ -24,30 +26,46 @@ def extract_area_land():
 def extract_gps_coordinates():
     pass
 
-def train_model():
-    pass
 
 
 class Appraisor:
 
-    def __init__(self,url,area,area_land,gps_lat,gps_lon,model_path):
-        pass
+    def __init__(self,url,model_path):
+        self.model_path = model_path
+        self.model = self.load_model(model_path)
+        self.url = url
+        re_dict = self.get_data_from_ad(url)
+        self.re_attributes = self.prepare_data(re_dict)
 
-    def load_model():
-        pass
+    def load_model(self,model_path):
+        from_file = catboost.CatBoostRegressor()
+        model = from_file.load_model(model_path)
+        return model
     
-    def get_data_from_ad(self):
-        pass
+    def get_data_from_ad(self,url):
+        re_d = get_real_estate_json(url)
+        return re_d
 
-    def prepare_data(self):
-        pass
+    def prepare_data(self,re_d):
+        area = extract_area(re_d)
+        area_land = extract_area_land(re_d)
+        gps_lat, gps_lon = extract_gps_coordinates(re_d) 
+        re_attributes = pd.Series({'area':area,'area_land':area_land, 'gps_lat':gps_lat, 'gps_lon': gps_lon})
+        return re_attributes
 
-    def predict(self):
-        pass
+    def predict(self,):
+        return self.model.predict(self.attributes)
 
 
 if __name__ == "__main__":
     # argument parsing from CLI
-    #app = Appraisor()
-    #app.predict()
-    get_real_estate_json()
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--url', 
+                        help='an integer for the accumulator')
+    parser.add_argument('--model_path')
+    args = parser.parse_args()
+    url = args.url
+    model_path = args.model_path
+
+    app = Appraisor(url, model_path)
+    app.predict()
